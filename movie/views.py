1c994235ceb1c38f730ef6ec8aabfe581ec60d4f
JuanesAo <140968790+JuanesAo@users.noreply.github.com>
2025-02-08 15:49:30 -0500

    Implementamos correctamente el about.html y ya se puede visualizar

--- movie/views.py
+++ movie/views.py
@@ -8,3 +8,6 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name':'Juan Esteban Alzate Ospina'})
+
+def about(request):
+    return render(request, 'about.html', {'name':'Juan Esteban Alzate Ospina'})
